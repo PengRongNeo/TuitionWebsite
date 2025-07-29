94c120e126d31c9fb4d0342166861287d296fb0c
Peng Rong Neo <pengrongneo@gmail.com>
2025-07-29 23:26:57 +0700

    edit statement

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -6,7 +6,7 @@
         <h2 class="title">Tuition @ Pioneer</h2>
         <p class="subtitle">
           We provide <strong>quality</strong> and <strong>affordable</strong> tuition
-          to help students improve academically and confidently.
+          to help students improve academically and gain confidence.
         </p>
         <a
           class="cta-outline-button"
